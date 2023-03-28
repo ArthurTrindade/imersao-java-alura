beed43ffc0355a461bc0f3829d88a58e0f02a8f0
ArthurTrindade <arthsilva500@gmail.com>
2023-03-28 14:32:16 -0300

    done: desafio 2

--- Aula 02 - Gerando Figurinhas parar o Whatsapp/src/App.java
+++ Aula 02 - Gerando Figurinhas parar o Whatsapp/src/App.java
@@ -1,5 +1,68 @@
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // url da API
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
+
+        // Fazendo o URI da URL
+        URI uri = URI.create(url);
+
+        // variavel com client HTTP
+        HttpClient client = HttpClient.newHttpClient();
+
+        // Fazendo a requisição HTTP (GET)
+        HttpRequest req = HttpRequest.newBuilder(uri).GET().build();
+    
+        // Executando a requisição HTTP sendo devolvida em string
+        HttpResponse<String> response = client.send(req, BodyHandlers.ofString());
+
+        String body = response.body();
+
+        // System.out.println(body);
+
+        /* Parseando os dados de body */
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body); 
+
+        /* Exibindo a API */
+
+        var sticket = new StickerGenerator();
+        for (Map<String, String> filme : listaDeFilmes) {
+            // printFormattedMovies(filme);
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = titulo + ".png";
+            
+            sticket.createSticker(inputStream, nomeArquivo);
+
+            System.out.println(titulo);
+            System.out.println();
+        }
+    }
+
+    static void printFormattedMovies(Map<String, String> movie) {
+        System.out.println(String.format("\u001b[1m %s \u001b[0m", movie.get("title")));
+        String rating = movie.get("imDbRating");
+        System.out.println(String.format("Notas: \u001b[32m %s \u001b[0m", rating));
+
+        var r = ((int)Double.parseDouble(rating));
+        for (int i = 0; i < r; i++) {
+            System.out.print("\uD83D\uDC99");
+        }
+
+        System.out.println();
+        System.out.println();
     }
 }

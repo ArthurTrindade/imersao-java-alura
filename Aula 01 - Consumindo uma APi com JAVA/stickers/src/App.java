ab912912aae185f0baa986e44ef100b880e9b4fc
ArthurTrindade <arthsilva500@gmail.com>
2023-03-28 10:51:38 -0300

    Aula 01

---
+++ Aula 01 - Consumindo uma APi com JAVA/stickers/src/App.java
@@ -0,0 +1,51 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // url da API
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
+
+        // Fazendo o URI da URL
+        URI uri = URI.create(url);
+
+        // variavel com client HTTP
+        HttpClient client = HttpClient.newHttpClient();
+
+        // Fazendo a requisição HTTP (GET)
+        HttpRequest req = HttpRequest.newBuilder(uri).GET().build();
+    
+        // Executando a requisição HTTP sendo devolvida em string
+        HttpResponse<String> response = client.send(req, BodyHandlers.ofString());
+
+        String body = response.body();
+
+        // System.out.println(body);
+
+        /* Parseando os dados de body */
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body); 
+
+        /* Exibindo a API */
+        for (Map<String, String> filme : listaDeFilmes) {
+            System.out.println(String.format("\u001b[1m %s \u001b[0m", filme.get("title")));
+            String rating = filme.get("imDbRating");
+            System.out.println(String.format("Notas: \u001b[32m %s \u001b[0m", rating));
+
+            var r = ((int)Double.parseDouble(rating));
+            for (int i = 0; i < r; i++) {
+                System.out.print("\uD83D\uDC99");
+            }
+            
+            System.out.println();
+            System.out.println();
+        }
+
+    }
+}
